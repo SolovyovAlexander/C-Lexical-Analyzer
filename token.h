bf20e21380474b542604b1fa8a0c8633eb0af9f0
Solovyov Alexander <AleksDaemon@yandex.ru>
2019-09-01 13:18:10 +0300

    added some operators

--- token.h
+++ token.h
@@ -4,7 +4,7 @@
 #endif //CC_COURSE_TOKENS_H
 
 
-//SIMPLE OPERATIONS
+//SIMPLE OPERATORS
 #define TK_CODE_NOT 0 // '!'
 #define TK_CODE_MOD 1 // '%'
 #define TK_CODE_POWER 2 // '^'
@@ -15,10 +15,28 @@
 #define TK_CODE_EQUAL 7 // '='
 #define TK_CODE_BITWISE_OR 8 // '|'
 #define TK_CODE_DIRECT_SELECTION 9 // '.'
-#define TK_CODE_LESS 9 // '<'
-#define TK_CODE_GREATER 9 // '>'
-#define TK_CODE_DIVISION 9 // '/'
-#define TK_CODE_QUESTION 9 // '?'
+#define TK_CODE_LESS 10 // '<'
+#define TK_CODE_GREATER 11 // '>'
+#define TK_CODE_DIVISION 12 // '/'
+#define TK_CODE_QUESTION 13 // '?'
+
+//COMPOUND ASSIGNMENT OPERATORS
+#define TK_CODE_PLUS_EQUAL 14 // '+='
+#define TK_CODE_MINUS_EQUAL 15 // '-='
+#define TK_CODE_MULTIPLICATION_EQUAL 16 // '*='
+#define TK_CODE_DIVISION_EQUAL 17 // '/='
+#define TK_CODE_MOD_EQUAL 18 // '%='
+#define TK_CODE_LESS_EQUAL 19 // '<='
+#define TK_CODE_GREATER_EQUAL 20 // '>='
+#define TK_CODE_AND_EQUAL 21 // '&='
+#define TK_CODE_POWER_EQUAL 22 // '^='
+#define TK_CODE_OR_EQUAL 22 // '|='
+#define TK_CODE_OR_EQUAL 22 // '|='
+
+
+
+
+//DELIMITERS
 
 
 
