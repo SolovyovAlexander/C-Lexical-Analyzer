d38b230fb260e20fdcf2eaa8ff1b7a22c9d831ae
Solovyov Alexander <AleksDaemon@yandex.ru>
2019-09-01 16:13:28 +0300

    added check for keyword

--- scanner.c
+++ scanner.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "token.h"
+#include "string.h"
 
 struct CToken* get_next_token(char* file_name) {
 
@@ -26,3 +27,24 @@ struct CToken* get_next_token(char* file_name) {
     fclose(fp);
     return 0;
 }
+
+void define_token(char* str){
+
+}
+
+int is_keyword(char* str){
+    char keywords[32][8] = {"auto","break","case","char","const","continue","default",
+                             "do","double","else","enum","extern","float","for","goto",
+                             "if","int","long","register","return","short","signed",
+                             "sizeof","static","struct","switch","typedef","union",
+                             "unsigned","void","volatile","while"};
+    for (int i = 0; i <32 ; ++i) {
+        if(strcmp(keywords[i],str) == 0){
+            return 1;
+        }
+    }
+    return 0;
+
+}
+
+int is_operator(){}
--- token.h
+++ token.h
@@ -19,24 +19,45 @@
 #define TK_CODE_GREATER 11 // '>'
 #define TK_CODE_DIVISION 12 // '/'
 #define TK_CODE_QUESTION 13 // '?'
+#define TK_CODE_TILDE 14 // '~'
 
 //COMPOUND ASSIGNMENT OPERATORS
-#define TK_CODE_PLUS_EQUAL 14 // '+='
-#define TK_CODE_MINUS_EQUAL 15 // '-='
-#define TK_CODE_MULTIPLICATION_EQUAL 16 // '*='
-#define TK_CODE_DIVISION_EQUAL 17 // '/='
-#define TK_CODE_MOD_EQUAL 18 // '%='
-#define TK_CODE_LESS_EQUAL 19 // '<='
-#define TK_CODE_GREATER_EQUAL 20 // '>='
-#define TK_CODE_AND_EQUAL 21 // '&='
-#define TK_CODE_POWER_EQUAL 22 // '^='
-#define TK_CODE_OR_EQUAL 22 // '|='
-#define TK_CODE_OR_EQUAL 22 // '|='
-
-
-
+#define TK_CODE_PLUS_EQUAL 15 // '+='
+#define TK_CODE_MINUS_EQUAL 16 // '-='
+#define TK_CODE_MULTIPLICATION_EQUAL 17 // '*='
+#define TK_CODE_DIVISION_EQUAL 18 // '/='
+#define TK_CODE_MOD_EQUAL 19 // '%='
+#define TK_CODE_LESS_EQUAL 20 // '<='
+#define TK_CODE_GREATER_EQUAL 21 // '>='
+#define TK_CODE_AND_EQUAL 22 // '&='
+#define TK_CODE_POWER_EQUAL 23 // '^='
+#define TK_CODE_OR_EQUAL 24 // '|='
+#define TK_CODE_INCREMENT 25 // '++'
+#define TK_CODE_DECREMENT 26 // '--'
+#define TK_CODE_SHIFT_LEFT 27 // '<<'
+#define TK_CODE_SHIFT_RIGHT 28 // '>>'
+#define TK_CODE_EQUAL_EQUAL 29 // '=='
+#define TK_CODE_NOT_EQUAL 30 // '!='
+#define TK_CODE_AND_AND 31 // '&&'
+#define TK_CODE_OR_OR 32 // '||'
+#define TK_CODE_IMPLICATION 33 // '-->'
+#define TK_CODE_SHIFT_LEFT_EQUAL 34 // '<<='
+#define TK_CODE_SHIFT_RIGHT_EQUAL 35 // '>>='
 
 //DELIMITERS
+#define TK_CODE_OPEN_BRACKET 36 // '('
+#define TK_CODE_CLOSE_BRACKET 38 // ')'
+#define TK_CODE_SQUARE_OPEN_BRACKET 39 // '['
+#define TK_CODE_SQUARE_CLOSE_BRACKET 40 // ']'
+#define TK_CODE_FIGURE_OPEN_BRACKET 41 // '{'
+#define TK_CODE_FIGURE_CLOSE_BRACKET 42 // '}'
+#define TK_CODE_COMMA 43 // ','
+#define TK_CODE_SEMICOLON 44 // ';'
+#define TK_CODE_COLON 45 // ':'
+#define TK_CODE_ELLIPSIS 46 // '...'
+
+//KEYWORDS
+
 
 
 
